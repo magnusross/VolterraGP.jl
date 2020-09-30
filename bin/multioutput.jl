1bb8087aca68f303917207bdba2099210046887f
Magnus <magnusaross@gmail.com>
2020-09-30 10:26:00 +0100

    add MO post and like, and noise per output

--- bin/multioutput.jl
+++ bin/multioutput.jl
@@ -1,20 +1,56 @@
 using Revise
 using VolterraGP
 using Plots
+using Flux
+using LinearAlgebra 
 
 
-D = 3
-C = 2
+D = 2
+C = 1
 P = 1
 
 # initialise
-X = collect(Float64, -5:0.1:5)
-Y = sin.(X) + 0.1*randn(size(X)[1])
+X = collect(Float64, -5:1.:5)
+Y = fill(Float64[], D)
+
+t = collect(Float64, -20:0.3:20)
+
+
+for i = 1:D
+    Y[i] = i * sin.(X) +  (i-1)*0.1 * randn(size(X)[1])
+end 
+
+
+
 data = Data(X, Y)
 
-dpars = DiffableParameters([0.1], rand(Float64, (D, sum(1:C), P)), [0.1])
-gp = GaussianProcess(threeEQs, D, C, P, data, dpars) 
+# dpars = DiffableParameters([0.1], ones(Float64, (D, sum(1:C), P)), [0.1])
+gp = GaussianProcess(threeEQs, D, C, P, data) 
 
-K = VolterraGP.fill_K(X, X, gp)
+# K = VolterraGP.fill_K(t, t, gp)
 
-heatmap(1:size(K)[1], 1:size(K)[1], K)
+# heatmap(K)
+
+
+
+
+opt = Flux.ADAM(0.1)
+its = 20
+
+# train
+for i in 1:its
+        grads = gradient(Flux.params(gp.dpars.σ, gp.dpars.G, gp.dpars.u)) do
+                            negloglikelihood(gp)
+        end
+    for p in (gp.dpars.σ, gp.dpars.G, gp.dpars.u)
+      Flux.Optimise.update!(opt, p, grads[p])
+    end
+    print("negloglike: ", negloglikelihood(gp), "\n")
+
+end 
+
+p = VolterraGP.posterior(t, gp)
+μ_arr = reshape(p[1], (size(t)[1], D))
+plot(t, μ_arr , layout=(2, 1))
+scatter!(X, hcat(data.Y...) , layout=(2,1))
+
--- src/gp.jl
+++ src/gp.jl
@@ -1,11 +1,11 @@
 const rng = MersenneTwister(1234)
 struct Data 
-    X::Array{Float64} 
-    Y::Array{Float64}
+    X::Array{Float64,1} 
+    Y::Array{Array{Float64,1},1} # 
 end
 
 struct DiffableParameters
-    σ::Array{Float64,1} # measurement noise
+    σ::Array{Float64,1} # measurement noise per output
     G::Array{Float64,3} # all smoothing kenrel paramters 
     u::Array{Float64,1} # u kernel paramters
 end
@@ -25,17 +25,17 @@ mutable struct GaussianProcess
 end
 
 GaussianProcess(base_kernel, D, C, P ) = GaussianProcess(base_kernel, D, C, P, missing, missing, missing, init_dpars(D, C, P))
-GaussianProcess(base_kernel, D, C, P, data ) = GaussianProcess(base_kernel, D, C, P, data , missing, missing, init_dpars(D, C, P))
-GaussianProcess(base_kernel, D, C, P, data, dpars) = GaussianProcess(base_kernel, D, C, P, data , missing, missing, dpars )
+GaussianProcess(base_kernel, D, C, P, data ) = GaussianProcess(base_kernel, D, C, P, data, missing, missing, init_dpars(D, C, P))
+GaussianProcess(base_kernel, D, C, P, data, dpars) = GaussianProcess(base_kernel, D, C, P, data, missing, missing, dpars)
 
 
 function init_dpars(D::Int64, C::Int64, P::Int64)::DiffableParameters
-    G = rand(rng, Float64, (D, sum(1:C), P))
-    DiffableParameters([0.1], G, [0.1])
+    G = ones(Float64, (D, sum(1:C), P))
+    DiffableParameters(0.1 * ones(D), G, [0.1])
 end
 
 
-function fill_sub_K(t::Array{Float64}, tp::Array{Float64}, d::Int64, dp::Int64, gp::GaussianProcess)::Array{Float64, 2}
+function fill_sub_K(t::Array{Float64}, tp::Array{Float64}, d::Int64, dp::Int64, gp::GaussianProcess)::Array{Float64,2}
     cat(map.((tpi -> 
             map.(ti ->
                 kernel(ti, tpi, d, dp, gp), 
@@ -52,37 +52,64 @@ function fill_K(t::Array{Float64}, tp::Array{Float64}, gp::GaussianProcess)
 end 
 
 
-function fill_sub_μ(t::Array{Float64}, d::Int64, gp::GaussianProcess)::Array{Float64, 1}
+function fill_sub_μ(t::Array{Float64}, d::Int64, gp::GaussianProcess)::Array{Float64,1}
 	map.(ti -> full_E(ti, d, gp), t)
 end
 
-function fill_μ(t::Array{Float64}, gp::GaussianProcess)::Array{Float64, 1}
+function fill_μ(t::Array{Float64}, gp::GaussianProcess)::Array{Float64,1}
     vcat(map.(di -> fill_sub_μ(t, di, gp), 1:gp.D)...)
 end
 
 
-function posterior1D(t::Array{Float64}, gp::GaussianProcess; jitter=1e-5)::Tuple{Array{Float64,1},Array{Float64, 2}}
-    Koo = fill_sub_K(gp.data.X, gp.data.X, 1, 1, gp) + gp.dpars.σ[1]^2*I
-    Kop = fill_sub_K(gp.data.X, t, 1, 1, gp)
-    Kpp = fill_sub_K(t, t, 1, 1, gp)
+function posterior(t::Array{Float64}, gp::GaussianProcess; jitter=1e-5)::Tuple{Array{Float64,1},Array{Float64,2}}
+    Σ = Diagonal(vcat([gp.dpars.σ[i] * ones(size(gp.data.X)[1]) for i in 1:gp.D]...))
 
-    μo = fill_sub_μ(gp.data.X, 1, gp)
-    μp = fill_sub_μ(t, 1, gp)
+    Koo = fill_K(gp.data.X, gp.data.X, gp) + Σ   # gp.dpars.σ[1]^2 * I 
+    Kop = fill_K(gp.data.X, t, gp)
+    Kpp = fill_K(t, t, gp)  
 
-    Loo = cholesky(Koo).L 
+    μo = fill_μ(gp.data.X, gp)
+    μp = fill_μ(t, gp)
 
-	μ_post = μp + Kop' * (Loo' \ (Loo \ (gp.data.Y - μo)))
-	
+    y = vcat(gp.data.Y...)
+    
+    Loo = cholesky(Koo).L
+    
+    μ_post = μp + Kop' * (Loo' \ (Loo \ (y - μo)))
+	# μ_post = Kop' * inv(Koo) * y
 	Lop = Loo \ Kop
 	K_post = Kpp - Lop' * Lop
 	
 	(μ_post, K_post) 
 end
 
-function negloglikelihood(gp::GaussianProcess)::Float64
-    K = fill_sub_K(gp.data.X, gp.data.X, 1, 1, gp) + gp.dpars.σ[1]^2*I
-    μ = fill_sub_μ(gp.data.X, 1, gp)
 
-    Kp = K + gp.dpars.σ[1].^2 * I
-	0.5 * ( gp.data.Y' * inv(Kp) * gp.data.Y + log(det(Kp)) + size(gp.data.Y)[1] * log(2 * π))
+
+function negloglikelihood(gp::GaussianProcess; jitter=1e-5)::Float64
+    Σ = Diagonal(vcat([gp.dpars.σ[i] * ones(size(gp.data.X)[1]) for i in 1:gp.D]...))
+
+    K = fill_K(gp.data.X, gp.data.X, gp) + Σ + jitter * I
+    μ = fill_μ(gp.data.X, gp)
+    
+    y = vcat(gp.data.Y...)
+
+	0.5 * ( y' * inv(K) * y + logdet(K) + size(y)[1] * log(2 * π))
 end
+
+# function posterior1D(t::Array{Float64}, gp::GaussianProcess; jitter=1e-5)::Tuple{Array{Float64,1},Array{Float64,2}}
+#     Koo = fill_sub_K(gp.data.X, gp.data.X, 1, 1, gp) + gp.dpars.σ[1]^2 * I
+#     Kop = fill_sub_K(gp.data.X, t, 1, 1, gp)
+#     Kpp = fill_sub_K(t, t, 1, 1, gp)
+
+#     μo = fill_sub_μ(gp.data.X, 1, gp)
+#     μp = fill_sub_μ(t, 1, gp)
+
+#     Loo = cholesky(Koo).L 
+
+# 	μ_post = μp + Kop' * (Loo' \ (Loo \ (gp.data.Y - μo)))
+    
+# 	Lop = Loo \ Kop
+# 	K_post = Kpp - Lop' * Lop
+    
+# 	(μ_post, K_post) 
+# end
