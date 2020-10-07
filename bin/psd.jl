f17c73e3c352d1f7c3405260cd75b2c562f1a3e4
Magnus <magnusaross@gmail.com>
2020-10-07 12:20:09 +0100

    improve psd probelm example

--- bin/psd.jl
+++ bin/psd.jl
@@ -5,26 +5,33 @@ using Plots
 
 
 D = 3
-C = 1 
+C = 2 
 P = 1
-data = Data(collect(0.:0.1:1.), fill(collect(0.:0.01:1.), D))
+
+data = Data(collect(0.:0.01:1.), fill(collect(0.:0.01:1.), D))
+
 for i in [0.0001, 0.001, 0.01, 0.1, 1.]
-    gp =  i * ones(Float64, (D, sum(1:C), P))
-    # gp[2, 1, 1] *= i
-    # gp[1, 3, 1] += i
+    
+    # run with C=2
+    Gpars =  i * ones(Float64, (D, sum(1:C), P))
+    
+    # run with C=1
+    # Gpars = ones(Float64, (D, sum(1:C), P))
+    # Gpars[2, 1, 1] -= i
 
-    dpars = DiffableParameters(fill(0.001, D),  gp,  [0.001])
 
+    dpars = DiffableParameters(fill(0.001, D),  Gpars,  [0.001])
 
     gp = GaussianProcess(threeEQs, D, C, P, data, dpars) 
-
+    
+    # noise per output
     Σ = Diagonal(vcat([gp.dpars.σ[i]^2 * ones(size(gp.data.X)[1]) for i in 1:gp.D]...))
     
     K = VolterraGP.fill_K(data.X, data.X, gp) + Σ + 1e-5 * I
-    plt = heatmap(K)
-    display(plt)
-    println(minimum(K))
-    # println(maximum(K))
+
+    # plt = heatmap(K)
+    # display(plt)
+
     println(minimum(eigvals(K)))
     println(maximum(K' - K))
 end 
